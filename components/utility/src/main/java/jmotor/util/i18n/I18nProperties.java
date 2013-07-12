84ef4c7e7c41e7d1363c10edfdf8a448ef9bbd52
Andy Ai <yanbo.ai@gmail.com>
2013-07-12 15:11:32 +0800

    Remarks: stove load method

--- components/utility/src/main/java/jmotor/util/i18n/I18nProperties.java
+++ components/utility/src/main/java/jmotor/util/i18n/I18nProperties.java
@@ -1,8 +1,11 @@
 package jmotor.util.i18n;
 
+import jmotor.util.StreamUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Locale;
 import java.util.Properties;
 
 /**
@@ -13,9 +16,21 @@ import java.util.Properties;
  * @author Andy.Ai
  */
 public class I18nProperties extends Properties {
+    public static final String SUFFIX = ".properties";
+
     @Override
     public void load(InputStream inStream) throws IOException {
         InputStreamReader reader = new InputStreamReader(inStream, "UTF-8");
         super.load(reader);
+    }
+
+    public void load(String name, Locale locale) throws IOException {
+        String _locale = locale.getLanguage() + "_" + locale.getCountry();
+        load(name, _locale);
+    }
+
+    public void load(String name, String locale) throws IOException {
+        String fileName = name + "_" + locale + SUFFIX;
+        load(StreamUtils.getStream4ClassPath(fileName));
     }
 }
